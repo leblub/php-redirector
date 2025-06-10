2b4d25850470584e51465b5cadddc52b99ab2388
leblub <76062765+leblub@users.noreply.github.com>
2025-06-10 14:04:45 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,20 +1,5 @@
 <?php
-// Datei: index.php
-
-$counter_file = 'counter.txt';
-
-// Wenn die Datei nicht existiert, initialisiere sie
-if (!file_exists($counter_file)) {
-    file_put_contents($counter_file, '0');
-}
-
-// Zähler auslesen und erhöhen
-$counter = (int)file_get_contents($counter_file);
-$next = ($counter % 2 === 0) ? 'https://twitch.tv/dein-kanal' : 'https://youtube.com/dein-kanal';
-
-// Zähler hochzählen und speichern
-file_put_contents($counter_file, $counter + 1);
-
-// Weiterleitung
-header("Location: $next");
+$urls = ['https://twitch.tv/', 'https://youtube.com/'];
+$random_url = $urls[array_rand($urls)];
+header("Location: $random_url");
 exit;
