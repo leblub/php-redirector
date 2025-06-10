1ea717b1ce15b2266ba627eb18fa0e8c92936880
leblub <76062765+leblub@users.noreply.github.com>
2025-06-10 17:11:42 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,5 +1,5 @@
 <?php
-$urls = ['https://twitch.tv/', 'https://youtube.com/'];
+$urls = ['https://docs.google.com/document/d/13REdgUPqEAtuctizVS-GOYWYH2vrgvgFWqlRQ-mcxr0/edit?usp=sharing', 'https://docs.google.com/document/d/1jt-9aifC3J-mathl8U40jtLBNgYbvm7ExOia7OdCvi4/edit?usp=sharing'];
 $random_url = $urls[array_rand($urls)];
 header("Location: $random_url");
 exit;
